814b4d12ae8509f8cfd246c893bcc4eb8292c262
JancyClaret16 <jancyclaret1605@gmail.com>
2026-06-26 11:24:48 +0530

    Add files via upload
    This program serves as a comprehensive guide for python string handling. It covers fundamental operations like slicing, indexing, reverse.

---
+++ String handling.py
@@ -0,0 +1,23 @@
+#creating and joining strings
+
+name="jancy"
+print(name)
+full_name=name+" claret"
+print("-----string concatenation-------")
+print (full_name)
+print("------string Repetation---------")
+print (name * 2)
+print("------string Indexing------")
+print (name[0])
+print("------string positive slicing------")
+print (name[0:2])
+print("------string Reverse------")
+print (full_name[::-1])
+print("------string negative slicing------")
+print (full_name[-6:])
+print("------string length------")
+print (len(full_name))
+
+#string supporting/string dotted functions
+
+
